667400bcbf9f865222e9b1f669d0365d30e4040c
Dave Shield <david.shield@gmail.com>
2018-02-18 00:13:05 +0000

    first commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const hbs = require("hbs");
+const fs = require("fs");
+
+var app = express();
+
+
+hbs.registerPartials(__dirname + "/views/partials");
+app.set("view engine", "hbs");
+
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFileSync("server.log", log + "\n");
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render("maintenance.hbs");
+// });
+
+app.use(express.static(__dirname + "/public"));
+
+hbs.registerHelper("getCurrentYear", () => {
+  return new Date().getFullYear()
+
+
+});
+
+app.get('/', (req, res) => {
+  res.render("home.hbs", {
+    pageTitle: "Homepage",
+    welcomeMessage: "Welcome to my page"
+  });
+});
+
+app.get("/bad", (req,res) => {
+  res.send({
+    errorMessage: "Bad Request"
+    
+  });
+
+});
+
+app.get("/about", (req, res) => {
+  res.render("about.hbs", {
+    pageTitle: "About Us"
+  });
+
+});
+
+
+
+app.listen(3000, ()=> {
+  console.log("Server is listining on port 3000")
+});
